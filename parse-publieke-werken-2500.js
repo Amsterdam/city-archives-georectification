7de171f00feefeba362d03014d166e17e3304e93
Bert Spaan <bert.spaan@gmail.com>
2019-02-20 13:47:35 +0100

    added publieke werken 1:2500 GCPs

---
+++ parse-publieke-werken-2500.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const H = require('highland')
+const csv = require('csv-parser')
+
+const io = require('./lib/io')
+const projections = require('./lib/projections')
+
+const csvOptions = {
+  separator: ';'
+}
+
+function makeGcp (row, prefix, height) {
+  const xw = parseFloat(row[`${prefix}xw`])
+  const yw = parseFloat(row[`${prefix}yw`])
+
+  // GCPs from CSV are in the old RD projection
+  // To convert to EPSG:28992, move the center of the projection to Amersfoort!
+  return {
+    world: projections.rdToLatLon([xw + 155000, yw + 463000]),
+    image: [parseFloat(row[`${prefix}xi`]), height - parseFloat(row[`${prefix}yi`])]
+  }
+}
+
+const rows = io.input.pipe(csv(csvOptions))
+
+H(rows)
+  .filter((row) => row.imgfn)
+  .map((row) => ({
+    id: parseInt(row.versienr),
+    sheet: row.kaartnr,
+    period: row.datering.split('-').map((year) => parseInt(year)),
+    // uitvoering: 'Druk in zwart. Geannoteerd',
+    // kleuren: 'zwart'
+    gcps: [
+      makeGcp(row, 'ul', parseInt(row.hoogte)),
+      makeGcp(row, 'll', parseInt(row.hoogte)),
+      makeGcp(row, 'ul', parseInt(row.hoogte)),
+      makeGcp(row, 'lr', parseInt(row.hoogte))
+    ],
+    imageId: row.imgfn,
+    scanWidth: parseInt(row.breedte),
+    scanHeight: parseInt(row.hoogte)
+  }))
+  .pipe(io.output)
--- parse-vele-handen.js
+++ parse-vele-handen.js
@@ -1,48 +1,28 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
 const H = require('highland')
 const R = require('ramda')
 const csv = require('csv-parser')
 
+const io = require('./lib/io')
+
 const LAT_LNG_DECIMALS = 6
-
-const argv = require('minimist')(process.argv.slice(2), {
-  alias: {
-    o: 'output',
-    t: 'type'
-  },
-  default: {
-    type: 'ndjson'
-  }
-})
-
-const geojson = {
-  open: '{"type": "FeatureCollection","features":[',
-  close: ']}'
-}
 
 const csvOptions = {
   separator: ';'
 }
 
-if (process.stdin.isTTY && !argv._[0]) {
-  console.error('Please specify location of CSV export')
-  process.exit(1)
+function roundDecimals (num, decimals) {
+  return Math.round(num * Math.pow(10, decimals)) / Math.pow(10, decimals)
 }
-
-const inputStream = ((argv._.length ? fs.createReadStream(argv._[0], 'utf8') : process.stdin))
-const outputStream = argv.o ? fs.createWriteStream(argv.o, 'utf8') : process.stdout
-const rowStream = inputStream.pipe(csv(csvOptions))
 
 function parseGCP (gcps) {
   const gcp = JSON.parse(gcps.replace(new RegExp('\\\\', 'g'), ''))
 
   return {
-    id: parseInt(gcp.id.replace('marker_', '')),
-    geo: [
-      roundDecimals(parseFloat(gcp.geoLat), LAT_LNG_DECIMALS),
+    world: [
       roundDecimals(parseFloat(gcp.geoLng), LAT_LNG_DECIMALS),
+      roundDecimals(parseFloat(gcp.geoLat), LAT_LNG_DECIMALS)
     ],
     image: [
       Math.round(parseFloat(gcp.imageGeoLat)),
@@ -51,60 +31,20 @@ function parseGCP (gcps) {
   }
 }
 
-function roundDecimals (num, decimals) {
-  return Math.round(num * Math.pow(10, decimals)) / Math.pow(10, decimals)
-}
+const rows = io.input.pipe(csv(csvOptions))
 
-function toFeature (map) {
-  return {
-    type: 'Feature',
-    properties: {
-      ...map,
-      gcps: map.gcps.map(R.omit(['geo']))
-    },
-    geometry: {
-      type: 'MultiPoint',
-      coordinates: map.gcps.map((gcp) => ([
-        gcp.geo[1], gcp.geo[0]
-      ]))
-    }
-  }
-}
-
-const maps = H(rowStream)
+H(rows)
   .map((row) => ({
-    ...row,
-    gcps: parseGCP(row.gcps)
+    veleHandenScanId: row.velehanden_scan_id,
+    imageId: row.memorix_id,
+    gcp: parseGCP(row.gcps)
   }))
-  .stopOnError((err) => {
-    console.error(err)
-  })
-  .group('velehanden_scan_id')
+  .group('veleHandenScanId')
   .map(R.toPairs)
   .sequence()
   .map((pair) => ({
-    ...pair[1][0],
-    gcps: pair[1].map(R.prop('gcps'))
+    imageId: pair[1][0].imageId,
+    veleHandenScanId: pair[1][0].veleHandenScanId,
+    gcps: pair[1].map(R.prop('gcp'))
   }))
-
-if (argv.type === 'ndjson') {
-  maps
-    .map(JSON.stringify)
-    .intersperse('\n')
-    .pipe(outputStream)
-} else if (argv.type === 'geojson') {
-  const features = maps
-    .map(toFeature)
-    .map(JSON.stringify)
-    .intersperse(',\n')
-
-  H([
-    H([geojson.open]),
-    features,
-    H([geojson.close])
-  ]).compact()
-    .sequence()
-    .pipe(outputStream)
-} else {
-  console.error('-t/--type argument must be either \'ndjson\' or \'geojson\'')
-}
+  .pipe(io.output)
